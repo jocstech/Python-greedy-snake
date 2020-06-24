dc4c83da2ae67ac44b041c9428c4b43523d06799
Joseph Fang <jocstech@gmail.coml>
2020-06-23 20:26:23 -0400

    update

--- cont.py
+++ cont.py
@@ -7,33 +7,36 @@ from pygame.locals import *
 
 # 全局参数初始化
 pygame.init()
-size = (840, 640)
+size = width,height = 840, 640
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
 black = pygame.Color(0, 0, 0)  # RGB
-white = pygame.Color(255, 255, 255)
+white = 255, 255, 255
 grey = pygame.Color(50, 50, 50)
-green = pygame.Color(0, 255, 0)
+green = 0, 255, 0
 
-snake_head_position = [20, 20]
+
+snake_head_position = [random.randrange(20, width), random.randrange(20, height)]
+snake_head_size = 20, 20
 snake_head_color = white
+snake_head = pygame.Rect(snake_head_position, snake_head_size)
+
+snake_body = [],
 snake_body_position = [[80, 20], [60, 20], [40,20]]
 snake_body_color = white
 
-def drawSnake(snake_head):
-    pygame.draw.rect(snake_head_position, snake_head_color)
-
-def drawSnake(snake_body):
-    pygame.draw.rect(snake_body_position, snake_body_color)
-
-food_position = (random.randrange(20, 820), random.randrange(20, 620))
+food_position = [random.randrange(20, width), random.randrange(20, height)]
 food_color = green
 food_flag = 1
 
+def drawSnake():
+  pygame.draw.rect(screen, snake_head_color, snake_head)
+
+
 def drawFood():
-    pygame.draw.rect(food_position, food_color)
-    
+  pygame.draw.rect(screen,food_color, food_position )
+
 '''def drawSnake(snake_body):
     for i in snake_body:
         pygame.draw.rect(screen, white, Rect(i[0], i[1], 20, 20))
@@ -41,61 +44,60 @@ def drawFood():
 def drawFood(food_position):
     pygame.draw.rect(screen, green, Rect(food_position[0], food_position, 20, 20))
 '''
+
+
+#边界碰撞返回
+def borderBounce():
+    if snake_head.left < 0 or snake_head.right > 840:
+        direction = -direction
+    if snake_head.top < 0 or snake_head.bottom > 640:
+        direction = -direction
+
+
 # 初始方向
 direction = 'right'
 
 # 游戏循环，移动
 while 1:
-    for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-
-            elif event.type == KEYDOWN:
-
-                if event.key == K_UP and direction != K_DOWN:
-                    direction = 'up'
-                if event.key == K_DOWN and direction != K_UP:
-                    direction = 'down'
-                if event.key == K_LEFT and direction != K_RIGHT:
-                    direction = 'left'
-                if event.key == K_RIGHT and direction != K_LEFT:
-                    direction = 'right'
-
-#边界碰撞返回
-def borderBounce():
-    if snake_head_position.left < 0 or snake_head_position.right > 840:
-        direction = -direction 
-    if snake_head_position.top < 0 or snake_head_position.bottom > 640:
-        direction = -direction 
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+    elif event.type == KEYDOWN:
+      if event.key == K_UP and direction != K_DOWN:
+        direction = 'up'
+      if event.key == K_DOWN and direction != K_UP:
+        direction = 'down'
+      if event.key == K_LEFT and direction != K_RIGHT:
+        direction = 'left'
+      if event.key == K_RIGHT and direction != K_LEFT:
+        direction = 'right'
 
 
-#判断是否吃掉食物
-if snake_head_position[0] == food_position[0] and snake_head_position[1] == food_position[1]:
-    food_flag = 0
-else:
-    snake_body_position.pop()
-#？为什么snake_head后面可以[]赋值？
+    #判断是否吃掉食物
+    if snake_head_position[0] == food_position[0] and snake_head_position[1] == food_position[1]:
+      food_flag = 0
+    else:
+      snake_body_position.pop()
+    #？为什么snake_head后面可以[]赋值？
 
-#生成新的食物
-if food_flag == 0:
-    x = random.randrange(1,32)
-    y = random.randrange(1,24)
-    food_position = [int(x*20),int(y*20)]
-    food_flag = 1
+    #生成新的食物
+    if food_flag == 0:
+        x = random.randrange(1,32)
+        y = random.randrange(1,24)
+        food_position = [int(x*20),int(y*20)]
+        food_flag = 1
 
+    screen.fill(black)
 
+    drawSnake()
 
-screen.fill(black)
+    # drawFood()
 
-drawSnake()
+    borderBounce()
 
-drawFood()
+    pygame.display.flip()
 
-borderBounce()
-
-pygame.display.flip()
-
-clock.tick(7)
+    clock.tick(7)
 
 
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 
 import sys  # 系统标准
 import random  # 随机函数
-import pygame # 游戏依赖
-import time
+import pygame  # 游戏依赖
+import pygame.freetype
+import datetime
 # 引入所需要的依赖库
 
 
 # 初始化游戏库
-pygame.init(
+pygame.init()
 # 定义窗口默认尺寸
 size = width, height = 841, 641  # 620 W x 440 H
 # 设置好相关参数
@@ -16,18 +17,25 @@ black = 0, 0, 0  # RGB
 white = 255, 255, 255
 grey = 50, 50, 50
 green = 0, 255, 0
+red = 255, 0, 0
 
 # 声明一个窗口对象
 screen = pygame.display.set_mode(size)
-clock = pygame.time.Clock() #控制游戏速度
-# 加载一个球的图片作为一个操控对象
-# ball = pygame.image.load("assets/ball.gif")
-# ball = pygame.transform.scale(ball, (50, 50))
-# ballreact = ball.get_rect()
+clock = pygame.time.Clock()  # 控制游戏速度
+GAME_FONT = pygame.freetype.Font("assets/front.ttf", 18)
+timer = 0
+fps = 120
+last_move = datetime.datetime.now().timestamp()
+time_gap = 0.2
+
+# 游戏记分系统
+total_score = 0
 
 
-#方格
+
+# 方格
 usize = 20
+
 
 def randomPosOnScreen():
     return (random.randrange(2, (width-1) / usize - 2) * usize + 1,
@@ -35,32 +43,35 @@ def randomPosOnScreen():
 
 
 initialized = False
-total_score = 0
 snack_head_velocity = 1 * usize
 snack_head_speed_vector = [snack_head_velocity, 0]
 snack_head_position = randomPosOnScreen()
 snack_head_size = (usize, usize)
 snack_head_color = white
 snack_head = pygame.Rect(snack_head_position, snack_head_size)
-#snack_body = []
+snack_body = []
+snack_body_length = 0
 
 food_color = green
 food_position = randomPosOnScreen()
 food = pygame.Rect(food_position, snack_head_size)
 
 
-def control(key):
-    print(key)
-    if(key[pygame.K_UP] or key[pygame.K_w]):
+def control(keyCode):
+  # 上键
+  if keyCode == 119 or keyCode == pygame.K_UP:
         snack_head_speed_vector[0] = 0
         snack_head_speed_vector[1] = -snack_head_velocity
-    if(key[pygame.K_DOWN] or key[pygame.K_s]):
+   # 下键
+  if keyCode == 115 or keyCode == pygame.K_DOWN:
         snack_head_speed_vector[0] = 0
         snack_head_speed_vector[1] = snack_head_velocity
-    if(key[pygame.K_LEFT] or key[pygame.K_a]):
+  # 左键
+  if keyCode == 97 or keyCode == pygame.K_LEFT:
         snack_head_speed_vector[1] = 0
         snack_head_speed_vector[0] = -snack_head_velocity
-    if(key[pygame.K_RIGHT] or key[pygame.K_d]):
+  # 右键
+  if keyCode == 100 or keyCode == pygame.K_RIGHT:
         snack_head_speed_vector[1] = 0
         snack_head_speed_vector[0] = snack_head_velocity
 
@@ -72,23 +83,28 @@ def placeFood():
         food_position = randomPosOnScreen()
     food.x, food.y = food_position
 
-
 # 边界碰撞判断
 def borderBounce():
-    if snack_head.left < 0 or snack_head.right > width:
-        snack_head_speed_vector[0] = -snack_head_speed_vector[0]
-    if snack_head.top < 0 or snack_head.bottom > height:
-        snack_head_speed_vector[1] = -snack_head_speed_vector[1]
+    if snack_head.left < 0:
+      snack_head_speed_vector[0] = abs(snack_head_speed_vector[0])
+    if snack_head.right > width:
+      snack_head_speed_vector[0] = -abs(snack_head_speed_vector[0])
+    if snack_head.top < 0:
+      snack_head_speed_vector[1] = abs(snack_head_speed_vector[1])
+    if snack_head.bottom > height:
+      snack_head_speed_vector[1] = -abs(snack_head_speed_vector[1])
+
+
+def growBody():
+  global snack_body_length
+  snack_body_length += 1
 
  # 碰撞判断
-
-
 def detectCollision():
     global total_score
-    if snack_head_position == food_position:
-        total_score += total_score
-        print('吃到了食物!')
-        print('总分:', total_score)
+    if snack_head.x == food.x and snack_head.y == food.y:
+        total_score += 1
+        growBody()
         placeFood()
 
 
@@ -99,18 +115,30 @@ def drawBackground():
     for y in range(0, height, usize):
         pygame.draw.aaline(screen, grey, (0, y), (width, y))
 
+def drawObjects():
+    for body in snack_body:
+      pygame.draw.rect(screen, snack_head_color, body)
+    pygame.draw.rect(screen, snack_head_color, snack_head)
+    pygame.draw.rect(screen, food_color, food)
+
+def moving():
+    # 移动蛇头
+    global snack_head,snack_body, last_move
+    if datetime.datetime.now().timestamp() - last_move > time_gap:
+      snack_body.insert(0,snack_head)
+      if len(snack_body) > snack_body_length:
+        snack_body.pop(-1)
+      snack_head = snack_head.move(snack_head_speed_vector)
+      snack_head_position = (snack_head.x, snack_head.y)
+      last_move = datetime.datetime.now().timestamp()
 
 # 初始化函数
-
-
 def initialization():
     global initialized, total_score
     if not initialized:
         total_score = 0
         placeFood()
         initialized = True
-
-timer = 0
 
 # 循环帧
 while 1:  # 1 = true
@@ -122,23 +150,23 @@ while 1:  # 1 = true
             sys.exit()
         if event.type == pygame.KEYDOWN:
             # 按键控制逻辑
-            control(event.type)
+            control(event.key)
+
 
     # 绘制画布
     drawBackground()
-    pygame.draw.rect(screen, snack_head_color, snack_head)
-    pygame.draw.rect(screen, food_color, food)
-
     # 边界碰撞判断
     borderBounce()
-
-    # 移动蛇头
-    snack_head = snack_head.move(snack_head_speed_vector)  
-    snack_head_position = (snack_head.x, snack_head.y)
+    # 绘制物体
+    drawObjects()
+    # 移动动作
+    moving()
     # 碰撞判断
     detectCollision()
-
+    # 绘制文字
+    GAME_FONT.render_to(screen, (20, 20), 'Total Score: ' + str(total_score), white)
+    GAME_FONT.render_to(screen, (20, 50), 'FPS: ' + str(fps), white)
     # 更新画面
     pygame.display.flip()
     # 绘制每一帧所需时间
-    timer += clock.tick(60)
+    timer += clock.tick(fps)
